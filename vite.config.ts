3d58cc4259beda1417b6fe855e91864d49cf12b7
AsRealTech <114587693+AsRealTech@users.noreply.github.com>
2026-04-29 06:08:35 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-// import path from "path";
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "src"),
-//       "@workspace/api-client-react": path.resolve(__dirname, "src/lib/api-client"),
-//     },
-//   },
-//   server: {
-//     port: import.meta.env.VITE_PORT, 
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:3001",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
